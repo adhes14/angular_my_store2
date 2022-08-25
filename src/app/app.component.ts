bff1e0ba74392487c03ebb3e406c6710df8c5e7d
Adhemar <adhemarduran@gmail.com>
2022-08-25 08:33:40 -0400

    create image component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = 'https://picsum.photos/200';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'The best toy',
+      price: 565,
+      image: './assets/images/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Almost new bike',
+      price: 852,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Album collection',
+      price: 565,
+      image: './assets/images/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'My books',
+      price: 654,
+      image: './assets/images/books.jpg'
+    },
+    {
+      id: '5',
+      name: 'Glasses to improve your vision',
+      price: 951,
+      image: './assets/images/glasses.jpg'
+    },
+    {
+      id: '6',
+      name: 'The house of your dreams',
+      price: 258,
+      image: './assets/images/house.jpg'
+    }
+  ];
 
   onLoaded(img: string) {
     console.log('From parent ', img);
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imgDefault: string = './assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    // Before render
+    // No run async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges() {
+    // bafore render - during changes
+    // eval input changes-- n changes on inputs
+    console.log('ngOnchanges', 'imgValue =>', this.img);
+  }
 
   ngOnInit(): void {
+    // Before render
+    // Could async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // children handler
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    // delete
+    console.log('ngOnDestroy');
   }
 
   imgError() {
