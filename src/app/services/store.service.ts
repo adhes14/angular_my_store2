1e6aafb530659d4aff4981b13a7bea1511b26c83
Adhemar <adhemarduran@gmail.com>
2022-08-26 20:06:50 -0400

    reactivity

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Product } from '../models/product.model';
 
 @Injectable({
@@ -7,6 +8,9 @@ import { Product } from '../models/product.model';
 export class StoreService {
 
   private myShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
+
+  myCart$ = this.myCart.asObservable();
 
   constructor() { }
 
@@ -16,6 +20,7 @@ export class StoreService {
 
   addProduct(product: Product) {
     this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart);
   }
 
   getTotal() {
